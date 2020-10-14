287ecde4cac79053148d186f27979b94001b4e78
wonjae Lee <wonjaelee99@gmail.com>
2020-10-14 10:09:16 +0900

    Initial Commit

---
+++ thai_new_creation_evangelism_paper_demo/thai_new_creation_evangelism_paper_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  thai_new_creation_evangelism_paper_demoApp.swift
+//  thai_new_creation_evangelism_paper_demo
+//
+//  Created by 이원재 on 10/14/20.
+//
+
+import SwiftUI
+
+@main
+struct thai_new_creation_evangelism_paper_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
